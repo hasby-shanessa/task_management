012868a5a3ad7c77d372039359f2a05b727d2568
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2025-11-20 16:05:49 +0200

    Setup and welcome message

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("============================================================");
+        System.out.println("||           Welcome Our Task Management System            ||");
+        System.out.println("============================================================");
+
+    }
+}
