9ad12ac4ce7ecada3a09887fecf62b12225e0a80
hasby-umutoniwabo <h.umutoniwa@alustudent.com>
2025-11-25 15:30:27 +0200

    added getters and a setter for projects

--- Project.java
+++ Project.java
@@ -1,6 +1,58 @@
+import java.lang.reflect.Array;
+
 public class Project {
     private static int IdCounter = 1;
+    private String projectId;
+    private String projectName;
+    private String projectDescription;
+    public enum projectType{
+        SOFTWARE,
+        HARDWARE
+    }
+    private projectType projectType;
+    private int teamSize;
+    private double budget;
+    private String status;
 
+    public Project(String projectName, String projectDescription, projectType projectType, int teamSize, double budget){
+        this.projectId = String.format("P%03d", IdCounter++);
+        this.projectName = projectName;
+        this.projectDescription = projectDescription;
+        this.projectType = projectType;
+        this.teamSize = teamSize;
+        this.budget = budget;
+        this.status = "Created";
+    }
+
+    // Getters to help return values of our strings
+
+    public String getProjectId(){
+        return projectId;
+    }
+    public String getProjectName(){
+        return projectName;
+    }
+    public String getProjectDescription(){
+        return projectDescription;
+    }
+    public projectType getProjectType(){
+        return projectType;
+    }
+    public int getTeamSize(){
+        return teamSize;
+    }
+    public double getBudget(){
+        return budget;
+    }
+    public String status(){
+        return status;
+    }
+
+    // status setter to help update our project's status
+
+    public void setStatus(String status){
+        this.status = status;
+    }
 
 
 //    public Project()
