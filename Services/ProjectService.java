abbcd42f6a7a44f9caa6a8bf119d9a3ef1df8906
hasby-shanessa <umutoniwabohasby@gmail.com>
2025-11-26 19:34:16 +0200

    added creating software project

--- Services/ProjectService.java
+++ Services/ProjectService.java
@@ -1,4 +1,29 @@
 package Services;
 
+import Models.Project;
+import Models.SoftwareProject;
+
 public class ProjectService {
+    //array to store all projects in the system
+    private Project[] projects;
+    private int projectCount;
+    private static final int MAX_PROJECTS = 100;
+
+    //Initialize empty project storage
+    public ProjectService(){
+        this.projects = new Project[MAX_PROJECTS];
+        this.projectCount = 0;
+    }
+
+    //Create software project
+    public SoftwareProject createSoftwareProject(String name, String projectDescription, int teamSize, String budget, String language, String framework, String repositoryUrl){
+        if(projectCount >= MAX_PROJECTS){
+            System.out.println("Error: Maximum projects reached (" + MAX_PROJECTS + ")");
+            return null;
+        }
+        SoftwareProject project = new SoftwareProject(name, projectDescription, teamSize, budget, language, framework, repositoryUrl);
+        projects[projectCount] = project;
+        projectCount++;
+        return project;
+    }
 }
