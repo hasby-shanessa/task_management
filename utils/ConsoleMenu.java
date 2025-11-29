2e0ffa5329383e20d799fa25246b8ecd86957605
hasby-shanessa <umutoniwabohasby@gmail.com>
2025-11-30 01:21:46 +0200

    project menu

--- utils/ConsoleMenu.java
+++ utils/ConsoleMenu.java
@@ -23,4 +23,22 @@ public class ConsoleMenu {
     public static int getMainMenuChoice(){
         return ValidationUtils.readIntInRange("Enter your choice: ",1,5);
     }
+    //display project menu
+    public static void displayProjectMenu(){
+        System.out.println();
+        System.out.println("****************************************************************");
+        System.out.println("|                      PROJECT MANAGEMENT                       |");
+        System.out.println("****************************************************************");
+        System.out.println();
+        System.out.println("1. View All Projects");
+        System.out.println("2. Create New Project");
+        System.out.println("3. View Project Details");
+        System.out.println("4. Delete Project");
+        System.out.println("5. Back to Main Menu");
+        System.out.println();
+
+    }
+    public static int getProjectMenuChoice(){
+        return ValidationUtils.readIntInRange("Enter your choice: ",1,5);
+    }
 }
