6797f9fdf93723909ff73b1a3a5df217011e4daf
hasby-shanessa <umutoniwabohasby@gmail.com>
2025-11-29 22:36:39 +0200

    read non empty string

--- utils/validationUtils.java
+++ utils/validationUtils.java
@@ -1,4 +1,21 @@
 package utils;
 
+import java.util.Scanner;
+
 public class validationUtils {
+    private static Scanner scanner = new Scanner(System.in);
+
+    //read non empty string
+    public static String readNonEmptyString(String prompt){
+        String input;
+        while (true){
+            System.out.println(prompt);
+            input = scanner.nextLine().trim();
+            if(input.isEmpty()){
+                System.out.println("Error: Input cannot be empty, please try again");
+            } else{
+                return input;
+            }
+        }
+    }
 }
